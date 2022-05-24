7982ef7dcd72ee7dd99383534c1704ad851750de
Wajahat Riaz <61377755+WajahatRiaz@users.noreply.github.com>
2022-05-25 01:04:37 +0500

    Master and Slave modules added

---
+++ RTL/Master.sv
@@ -0,0 +1,75 @@
+module Master (
+input 	logic clk, reset, start,
+input 	logic slaveSelect,
+input 	logic [7:0] masterDataToSend,
+output	logic [7:0] masterDataReceived,
+output  logic sclk,
+output  logic CS,
+
+output  logic MOSI_0,
+output  logic MOSI_1,
+output  logic MOSI_2,
+output  logic MOSI_3,
+
+input 	logic MISO_0,
+input	logic MISO_1,
+input 	logic MISO_2,
+input 	logic MISO_3);
+
+reg [7:0] Register; // Master shift register
+
+assign sclk = clk; // sclk to be sent to external flash memory
+
+reg transmit = 0; // To allow transmitting of bits
+
+integer maxCount = 0; // Counter that indicates transfer is complete when max is reached i.e. 2
+
+always @(posedge start or posedge reset) begin // Reset - Start
+if (!transmit && start) begin
+	Register <= masterDataToSend; // Initialize shift register
+	masterDataReceived <= 8'bxxxxxxxx; // Initialize data received with dont cares
+	maxCount <= 0; // Initial value of count
+	transmit <= 1; // Allow transmission between master & slave
+	
+	if (slaveSelect == 0) begin
+		CS <= 1'b0; // slave selected
+	end
+	
+	else begin
+		CS <= 1'b1; // No slave selected
+	end
+end
+
+if (reset) begin
+	Register <= 0; // Reset the register to zero
+	end
+end
+
+always @(posedge clk) begin // Shifting
+if (transmit) begin
+	Register <= Register >> 4; // Shift 4 times 
+	MOSI_0 <= Register[0]; // Write to MOSI 0
+	MOSI_1 <= Register[1]; // Write to MOSI 1
+	MOSI_2 <= Register[2]; // Write to MOSI 2
+	MOSI_3 <= Register[3]; // Write to MOSI 3
+
+	end
+end
+
+always @(negedge clk) begin // Sampling
+	if (maxCount >= 2) begin // Max count reached (All bits transferred)
+	transmit <= 0; // Disallow transmission
+	CS <= 1'b1; // Unselect slave
+	end
+
+if (transmit) begin
+	Register[7] <= MISO_0; // Read from MISO 0
+	Register[6] <= MISO_1; // Read from MISO 1
+	Register[5] <= MISO_2; // Read from MISO 2
+	Register[4] <= MISO_3; // Read from MISO 3
+
+	masterDataReceived <= {MISO_3,MISO_2,MISO_1,MISO_0, masterDataReceived[7:4]}; // Update Data Received from slave
+	maxCount <= maxCount + 1; // Increment counter
+	end
+end
+endmodule
---
+++ RTL/Slave.sv
@@ -0,0 +1,61 @@
+module Slave(
+input 	logic reset,
+input	logic [7:0] slaveDataToSend, 
+output  logic [7:0] slaveDataReceived,
+input	logic sclk,
+input	logic CS,
+input	logic MOSI_0,
+input 	logic MOSI_1,
+input 	logic MOSI_2,
+input 	logic MOSI_3,
+output  logic MISO_0,
+output  logic MISO_1,
+output  logic MISO_2,
+output  logic MISO_3);
+
+reg [7:0] Register; // Slave shift register
+
+reg flag = 0; // Boolean: To allow transmitting of bits
+
+always @(negedge CS or posedge reset) begin
+if (CS == 0) begin
+	Register <= slaveDataToSend; // Initialize shift register
+	slaveDataReceived <= 8'bxxxxxxxx; // Initialize data received with dont cares
+	flag <= 1; // Set flag to 1
+	end
+if (reset) begin
+	Register <= 0; // Reset shift register
+	end
+end
+
+always @(posedge CS) begin // When slave is unselected
+	flag <= 0; // Disallow transmission by setting flag to 0
+	MISO_0 <= 1'bz; // Disconnect MISO 0 from this slave
+	MISO_1 <= 1'bz; // Disconnect MISO 1 from this slave
+	MISO_2 <= 1'bz; // Disconnect MISO 2 from this slave
+	MISO_3 <= 1'bz; // Disconnect MISO 3 from this slave
+
+end
+
+always @(posedge sclk) begin// Shifting
+if (flag) begin
+	Register <= Register >> 4; // Shift
+	MISO_0<= Register[0]; // Write to MISO 0
+	MISO_1<= Register[1]; // Write to MISO 1
+	MISO_2<= Register[2]; // Write to MISO 2
+	MISO_3<= Register[3]; // Write to MISO 3
+	end
+end
+
+always @ (negedge sclk) begin// Sampling
+if (flag) begin
+	Register[7] = MOSI_0; // Read from MOSI 0
+	Register[6] = MOSI_1; // Read from MOSI 1
+	Register[5] = MOSI_2; // Read from MOSI 2
+	Register[4] = MOSI_3; // Read from MOSI 3
+
+	slaveDataReceived <= {MOSI_3,MOSI_2,MOSI_1,MOSI_0, slaveDataReceived[7:4]}; // Update Data received from master
+	end
+end
+
+endmodule
